f4fc977127d4bcfce99e4f07e51feff60e103d46
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-16 02:06:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(array)
+  new_array = []
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    strin = "The line is currently: "
+    people_in_line = 0
+    loop do
+        if people_in_line < array.size
+            strin << "#{people_in_line + 1}. #{array[people_in_line]} "
+            people_in_line += 1
+        else
+            break
+        end
+    end
+     puts strin.chop
+  end
+end
+
+def take_a_number(array, person)
+  new_array = []
+  new_array.push(person)
+  puts "Welcome, #{person}. You are number #{(person.index(person))+1} in line."
+end
